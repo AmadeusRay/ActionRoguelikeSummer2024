4a3d4a289f0493b9ba07bdba6c5f72bb0812ad4f
amadeusRay <134473121+amadeusRay@users.noreply.github.com>
2024-07-06 14:59:41 -0700

    Networking the treasure chest
    Client and Server sync - Interaction of item chest example
    Opening and closing the treasure chest shows up on both client and server

--- Source/ActionRoguelike/Private/SItemChest.cpp
+++ Source/ActionRoguelike/Private/SItemChest.cpp
@@ -3,7 +3,7 @@
 
 #include "SItemChest.h"
 #include "Components/StaticMeshComponent.h"
-
+#include "Net/UnrealNetwork.h"
 
 
 ASItemChest::ASItemChest()
@@ -15,10 +15,31 @@ ASItemChest::ASItemChest()
 	LidMesh->SetupAttachment(BaseMesh);
 
 	TargetPitch = 110;
+
+	SetReplicates(true);
 }
 
 
 void ASItemChest::Interact_Implementation(APawn* InstigatorPawn)
 {
-	LidMesh->SetRelativeRotation(FRotator(TargetPitch, 0, 0));
+	bLidOpened = !bLidOpened;
+	OnRep_LidOpened(); // manually calling to server. Although it shows it for both client and server interacting.
+	// In the course he had to add this.
+
+	
+}
+
+void ASItemChest::OnRep_LidOpened()
+{
+	float CurrPitch = bLidOpened ? TargetPitch : 0.0f;
+	LidMesh->SetRelativeRotation(FRotator(CurrPitch, 0, 0));
+}
+
+void ASItemChest::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ASItemChest, bLidOpened);
+
+	
 }
